919b1354521ea9b412ed39fcbfb36c3ab8acd565
Robert <poprobert1999@yahoo.com>
2024-05-03 14:34:55 +0300

    First commit

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,21 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+make_coffee = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+while True:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+    if choice == "off":
+        print("The coffee machine will turn off!")
+        break
+    elif choice == "report":
+        make_coffee.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if make_coffee.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            make_coffee.make_coffee(drink)
